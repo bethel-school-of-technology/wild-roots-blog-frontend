22cf298858f5bce24c6c4714885fa7ea9d7c55c7
complete-beep <57547792+complete-beep@users.noreply.github.com>
2021-01-09 15:57:13 -0600

    added scripture cards & updated css

---
+++ wild-roots-blog/src/assets/verses.js
@@ -0,0 +1,23 @@
+const vs=[
+    {
+        reference: "Proverbs 3:5-6",
+        verse: "Trust in the Lord with all your heart and lean not on your own understanding"
+    },
+    {
+        reference: "John 14:6",
+        verse: "Jesus said to him, “I am the way, the truth, and the life. No one comes to the Father except through Me."
+    },
+    {
+        reference: "Proverbs 13:3",
+        verse: "He who guards his mouth preserves his life, But he who opens wide his lips shall have destruction."
+    },
+    {
+        reference: "Revelation 3:20",
+        verse: "Behold, I stand at the door and knock. If anyone hears My voice and opens the door, I will come in to him and dine with him, and he with Me."
+    },
+    {
+        reference: "Colossians 3:17",
+        verse: "And whatever you do in word or deed, do all in the name of the Lord Jesus, giving thanks to God the Father through Him."
+    }
+]
+export default vs
